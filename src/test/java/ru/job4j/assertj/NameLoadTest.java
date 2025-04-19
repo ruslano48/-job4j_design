9af0b60cc9e0563d96e81cddbe84866a9fde3ea7
user <ruslano48@yandex.ru>
2025-04-19 14:32:23 +0300

    2. Подключение библиотеки AssertJ [#504881]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkIndexof() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("key="))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a value");
+    }
+
+    @Test
+    void checkStartsWithEqual() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("="))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a key");
+    }
+
+    @Test
+    void checkNameDoesNotContainEqualSign() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("keyvalue"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain the symbol '='");
+    }
+}
