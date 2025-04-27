c207466fa06446d98e3afaf453f9cde8061b9d0e
user <ruslano48@yandex.ru>
2025-04-27 09:31:50 +0300

    2. Реализовать Store<T extends Base> [#157 #541433]

--- src/main/java/ru/job4j/generic/Role.java
+++ src/main/java/ru/job4j/generic/Role.java
@@ -2,10 +2,12 @@ package ru.job4j.generic;
 
 public class Role extends Base {
     private final String roleName;
+
     public Role(String id, String roleName) {
         super(id);
         this.roleName = roleName;
     }
+
     public String getRoleName() {
         return roleName;
     }
