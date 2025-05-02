b9559006062922d16b2de79b631c41f593597a14
user <ruslano48@yandex.ru>
2025-05-02 16:32:15 +0300

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,87 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capasity) {
+        container = (T[]) new Object[capasity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length) {
+            grow();
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        T oldValue = get(index);
+        container[index] = newValue;
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, size);
+        T removed = container[index];
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(container, index + 1, container, index, numMoved);
+        }
+        container[--size] = null;
+        modCount++;
+        return removed;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private void grow() {
+        int newCapacity = container.length == 0 ? 1 : container.length * 2;
+        container = Arrays.copyOf(container, newCapacity);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int index = 0;
+            private final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                checkModCount();
+                return index < size;
+            }
+
+            @Override
+            public T next() {
+                checkModCount();
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[index++];
+            }
+
+            private void checkModCount() {
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+            }
+        };
+    }
+}
