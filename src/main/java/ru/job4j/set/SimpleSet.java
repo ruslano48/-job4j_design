667e25d43467d55459c91691919002f56f1dfd79
user <ruslano48@yandex.ru>
2025-05-19 19:43:56 +0300

    1. Реализовать коллекцию Set на массиве [#996]

--- src/main/java/ru/job4j/set/SimpleSet.java
+++ src/main/java/ru/job4j/set/SimpleSet.java
@@ -1,7 +1,12 @@
 package ru.job4j.set;
 
-public interface SimpleSet<T> extends Iterable<T> {
+import java.util.Iterator;
+
+public interface SimpleSet<T> {
 
     boolean add(T value);
+
     boolean contains(T value);
+
+    Iterator<T> iterator();
 }
